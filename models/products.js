f28f30710866498d08c61d0efc22153d6731342f
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-07-17 12:46:06 +0530

    remove varients from products and modified

--- models/products.js
+++ models/products.js
@@ -2,40 +2,69 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // require('mongoose-currency').loadType(mongoose);
 
-var attributeSchema = new Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    value:{
-        type: String
+// var attributeSchema = new Schema({
+//     name:{
+//         type: String,
+//         required: true
+//     },
+//     value:{
+//         type: String
        
-    }
-},{
-    timestamps: true
-});
+//     }
+// },{
+//     timestamps: true
+// });
 
-var varientSchema = new Schema({
-    price: {
-        type: Number,
-        required: true
-    },
-    availability:{
-        type: Number,
-        required: true
-    },
-    attributes: [attributeSchema],
-    sales: {
-        type: Number, 
-        default: 0
-    }
-}, {
-    timestamps: true
-});
+// var varientSchema = new Schema({
+//     price: {
+//         type: Number,
+//         required: true
+//     },
+//     availability:{
+//         type: Number,
+//         required: true
+//     },
+//     attributes: [attributeSchema],
+//     sales: {
+//         type: Number, 
+//         default: 0
+//     }
+// }, {
+//     timestamps: true
+// });
 
+
+// var productSchema = new Schema({
+//     brand:{
+//         type: String,
+//         required:true
+//     },
+//     variety:{
+//         type: mongoose.Schema.Types.ObjectId, 
+//         ref: 'Variety'
+//     },
+//     category:{
+//         type: mongoose.Schema.Types.ObjectId, 
+//         ref: 'Category'
+//     },
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     image: {
+//         type: String
+//     },
+//     varients: [varientSchema],
+//     sales: {
+//         type: Number, 
+//         default: 0
+//     }
+// }, {
+//     timestamps: true
+// });
 
 var productSchema = new Schema({
-    brand:{
+    productCode:{
         type: String,
         required:true
     },
@@ -47,14 +76,25 @@ var productSchema = new Schema({
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Category'
     },
-    name: {
+    carotSize: {
         type: String,
         required: true
     },
     image: {
         type: String
     },
-    varients: [varientSchema],
+    price:{
+        type: Number,
+        required: true
+    },
+    availability: {
+        type:Number,
+        required:true
+    },
+    materialUsed:{
+        type: String,
+        required: true
+    },
     sales: {
         type: Number, 
         default: 0
@@ -62,6 +102,7 @@ var productSchema = new Schema({
 }, {
     timestamps: true
 });
+
 
 
 var Products = mongoose.model('Product', productSchema);
