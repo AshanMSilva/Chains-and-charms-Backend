eabef92114c98cb9b16ff36b661e0acce529ce23
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-07-25 00:09:59 +0530

    config added

--- config.js
+++ config.js
@@ -9,8 +9,10 @@ module.exports = {
     'secretKey': config.get('secretKey'),
     // 'mongoUrl' : 'mongodb+srv://e_com_user:User123@chainsandcharms.hrmyt.mongodb.net/Chains-and-charms-Server?retryWrites=true&w=majority',
     'mongoUrl' : config.get('mongoUrl'),
-    'businessEmail':'smanalysis.uom@gmail.com',
-    'businessPassword': '123@Ashan@123'
+    'businessEmail': config.get('businessEmail'),
+    'businessPassword': config.get('businessPassword')
+    // 'businessEmail':'smanalysis.uom@gmail.com',
+    // 'businessPassword': '123@Ashan@123'
     // 'facebook': {
     //     clientId: '506662936665383',
     //     clientSecret: '70803f1359b6db7064fd5854c76c7803'
