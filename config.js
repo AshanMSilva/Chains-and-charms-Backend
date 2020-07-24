fda61cf6a19009a0b5cfe7f7521ea24f9a8987ca
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-07-24 20:20:09 +0530

    deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    // 'mongoUrl' : 'mongodb+srv://e_com_user:User123@chainsandcharms.hrmyt.mongodb.net/Chains-and-charms-Server?retryWrites=true&w=majority',
-    'mongoUrl' : 'mongodb://localhost:27017/Chains-and-charms-Server',
+    'mongoUrl' : 'mongodb+srv://e_com_user:User123@chainsandcharms.hrmyt.mongodb.net/Chains-and-charms-Server?retryWrites=true&w=majority',
+    // 'mongoUrl' : 'mongodb://localhost:27017/Chains-and-charms-Server',
     'businessEmail':'smanalysis.uom@gmail.com',
     'businessPassword': '123@Ashan@123'
     // 'facebook': {
