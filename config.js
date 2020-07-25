754d5965d1302338a2dc86e9873b9b454ccee37d
SahanJayasinghe <47143531+SahanJayasinghe@users.noreply.github.com>
2020-07-25 11:08:04 +0530

    config error resolved

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = require('config');
 
-if (!config.get('e_com_secretKey')) {
+if (!config.get('secretKey')) {
     throw new Error('FATAL ERROR !!! : jwt Secret Key is not defined.');
     // process.exit(1);
 }
 
 module.exports = {    
-    'secretKey': config.get('e_com_secretKey'),
-    'mongoUrl': config.get('e_com_mongoUrl'),
-    'businessEmail': config.get('e_com_businessEmail'),
-    'businessPassword': config.get('e_com_businessPassword')
+    'secretKey': config.get('secretKey'),
+    'mongoUrl': config.get('mongoUrl'),
+    'businessEmail': config.get('businessEmail'),
+    'businessPassword': config.get('businessPassword')
 }
