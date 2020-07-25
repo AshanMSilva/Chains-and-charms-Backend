43eaaa10e1c34f33208e638c6b6d2d69dbc023ff
SahanJayasinghe <47143531+SahanJayasinghe@users.noreply.github.com>
2020-07-25 10:54:26 +0530

    config env vars

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 const config = require('config');
 
-if (!config.get('secretKey')) {
+if (!config.get('e_com_secretKey')) {
     throw new Error('FATAL ERROR !!! : jwt Secret Key is not defined.');
     // process.exit(1);
 }
 
-module.exports = {
-    
-    'secretKey': process.env.secretKey,
-    'mongoUrl': process.env.mongoUrl,
-    'businessEmail': process.env.businessEmail,
-    'businessPassword': process.env.businessPassword
+module.exports = {    
+    'secretKey': config.get('e_com_secretKey'),
+    'mongoUrl': config.get('e_com_mongoUrl'),
+    'businessEmail': config.get('e_com_businessEmail'),
+    'businessPassword': config.get('e_com_businessPassword')
 }
